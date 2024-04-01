53392c4b0c761899b9014143471e866386d5c392
TollanBerhanu <tollanberhanu@gmail.com>
2024-04-01 16:53:47 +0300

    feat: add models - add serializers - register to admin site

--- api/models.py
+++ api/models.py
@@ -1,3 +1,33 @@
 from django.db import models
 
-# Create your models here.
+class Topic(models.Model):
+    topic_name = models.CharField(max_length=100)
+    topic_file = models.FileField(upload_to="bio_atomspace", null=True)
+
+    def __str__(self) -> str:
+        return self.topic_name
+
+class Chat(models.Model):
+    topic_id = models.ForeignKey(Topic, on_delete=models.CASCADE)
+    chat_name = models.CharField(max_length=100)
+    chat_created_on = models.DateField(auto_now_add=True)
+
+    def __str__(self) -> str:
+        return self.chat_name
+
+class Message(models.Model):
+    chat_id = models.ForeignKey(Chat, on_delete=models.CASCADE)
+    message_text = models.CharField(max_length=2000)
+    message_time = models.DateField(auto_now_add=True)
+    is_user_message = models.BooleanField(default=True)
+
+    def __str__(self) -> str:
+        return 'User Message' if self.is_user_message else 'LLM Message'
+
+class Example(models.Model):
+    topic_id = models.ForeignKey(Topic, on_delete=models.CASCADE)
+    example_title = models.CharField(max_length=100)
+    example_question = models.CharField(max_length=900)
+    
+    def __str__(self) -> str:
+        return self.example_title
---
+++ api/serializers.py
@@ -0,0 +1,22 @@
+from rest_framework import serializers
+from .models import *
+
+class TopicSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Topic
+        fields = '__all__'
+
+class ChatSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Chat
+        fields = '__all__'
+
+class MessageSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Message
+        fields = '__all__'
+
+class ExampleSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Example
+        fields = '__all__'
