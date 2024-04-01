e3824a27dcaaab87ed10cb426e6f975b26aae1f0
Tollan Sitotaw <tollanberhanu@gmail.com>
2024-04-02 00:23:35 +0300

    refactor: remove unnecessary url paths

--- api/urls.py
+++ api/urls.py
@@ -5,13 +5,13 @@ urlpatterns = [
     path('topics/', TopicList.as_view()),
     path('topics/<int:pk>/', TopicDetail.as_view()),
 
-    path('topics/chats/', ChatListAll.as_view()),
+    path('chats/', ChatListAll.as_view()),
     path('topics/<int:topic_id>/chats/', ChatList.as_view()),
-    path('topics/<int:topic_id>/chats/<int:chat_id>/', ChatDetail.as_view()), # TODO: This is not necessary
+    path('chats/<int:chat_id>/', ChatDetail.as_view()), # TODO: This is not necessary
 
     path('topics/<int:topic_id>/chats/<int:chat_id>/messages/', MessageList.as_view()),
-    path('topics/<int:topic_id>/chats/<int:chat_id>/messages/<int:message_id>/', MessageDetail.as_view()), # TODO: This is not necessary
+    path('messages/<int:message_id>/', MessageDetail.as_view()), # TODO: This is not necessary
 
     path('topics/<int:topic_id>/examples/', ExampleList.as_view()),
-    path('topics/<int:topic_id>/examples/<int:example_id>/', ExampleDetail.as_view()), # TODO: This is not necessary
+    path('examples/<int:example_id>/', ExampleDetail.as_view()), # TODO: This is not necessary
 ]
