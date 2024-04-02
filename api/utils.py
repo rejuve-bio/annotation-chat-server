0418574e51de98a5490bc08d813e9a79105cab4a
TollanBerhanu <tollanberhanu@gmail.com>
2024-04-02 12:49:37 +0300

    feat: add generic utility functions - get_records -get_paginated_records refactor: update record_exists function

--- api/utils.py
+++ api/utils.py
@@ -3,25 +3,8 @@ from rest_framework import status
 from .models import *
 
 # Check if the id exists in the database
-def check_id_exists(
-        topic_id=None,
-        chat_id=None,
-        message_id=None,
-        example_id=None):
-    
-    if topic_id and not Topic.objects.filter(pk=topic_id).exists():
-        return False, 'Invaild Topic ID!'
-    
-    if chat_id and not Chat.objects.filter(pk=chat_id).exists():
-        return False, 'Invaild Chat ID!'
-    
-    if message_id and not Chat.objects.filter(pk=message_id).exists():
-        return False, 'Invaild Message ID!'
-    
-    if example_id and not Chat.objects.filter(pk=example_id).exists():
-        return False, 'Invaild Example ID!'
-
-    return True, 'Valid!'
+def record_exists(record_model, record_id):
+    return record_model.objects.filter(pk=record_id).exists()
 
 # Update only the fields present in update_data
 def update_record(record_instance, update_data):
@@ -29,3 +12,32 @@ def update_record(record_instance, update_data):
         for key, value in update_data.items():
             setattr(record_instance, key, value)
         record_instance.save()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+    else:
+        return Response('Record does not exist!' ,status=status.HTTP_404_NOT_FOUND)
+
+def add_record(record_data, record_model, record_serializer, foreign_key=None):
+    if foreign_key is not None:
+        record_data.update(foreign_key)
+        # for key, value in foreign_key:
+        #     if record_exists(record_model=record_model, record_id=)
+    
+    serialized_record = record_serializer(data=record_data)
+
+    if serialized_record.is_valid():
+        created_record = record_model.objects.create(**serialized_record.validated_data)
+        serialized_record = record_serializer(created_record)
+        return Response(serialized_record.data, status=status.HTTP_201_CREATED)
+    else:
+        return Response(serialized_record.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+def get_records(filtered_records, record_serializer):
+    serialized_records = record_serializer(filtered_records, many=True).data
+    return Response(serialized_records, status=status.HTTP_200_OK)
+
+def get_paginated_records(pagination_class, request, record_items, record_serializer_class):
+    paginator = pagination_class()
+    result_page = paginator.paginate_queryset(record_items, request)
+    serialized_topics = record_serializer_class(result_page, many=True)
+    return paginator.get_paginated_response(serialized_topics.data)
